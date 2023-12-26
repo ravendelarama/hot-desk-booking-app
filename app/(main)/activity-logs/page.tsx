e38940d6236a25229c8a138df7c0b222a75aac3f
Raven <official.ravendelarama@gmail.com>
2023-12-26 10:49:52 +0800

    Fix ES Lint error

--- app/(main)/activity-logs/page.tsx
+++ app/(main)/activity-logs/page.tsx
@@ -8,7 +8,7 @@ async function ActivityLogs() {
     <div className="p-3 sm:pt-10 sm:pl-10 flex flex-col space-y-5">
       <h1 className="text-3xl font-bold font-sans">Activity Logs</h1>
       <h2 className="text-slate-500 text-sm">
-        It shows every users' activity logs
+        It shows every users&apos; activity logs
       </h2>
       <DataTable columns={columns} data={logs} />
     </div>
